bd1ae6a57a4fb1de6a07a545b86ecdbad400c83b
Prince Patrick Bondzie <ngekow@gmail.com>
2022-01-13 13:45:07 +0000

    database file  update

--- config/database.js
+++ config/database.js
@@ -1,16 +1,14 @@
 const mongoose = require("mongoose");
 
-mongoose
-  .connect("mongodb://localhost:27017/DentClic")
-  .then(() => console.log("Database connected successffully!"))
-  .catch((err) => console.log(err.message));
-
-  // mongoose
-//   .connect(process.env.MONGODB_URI, {
-//     useCreateIndex: true,
-//     useFindAndModify: false,
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() => console.log("Database connected successfully!"))
+// mongoose
+//   .connect("mongodb://localhost:27017/DentClic")
+//   .then(() => console.log("Database connected successffully!"))
 //   .catch((err) => console.log(err.message));
+
+  mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Database connected successfully!"))
+  .catch((err) => console.log(err.message));
